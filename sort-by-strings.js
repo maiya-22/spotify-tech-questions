30bbaf664d036da0f7aa8057cf0ba3de6c02836b
maiya-22 <maiya_o@yahoo.com>
2018-06-16 23:11:00 -0400

    comments updated

--- sort-by-strings.js
+++ sort-by-strings.js
@@ -2,7 +2,7 @@
 
 // I tried to make the variable names self-documenting.
 // Used the built-in JS sort method because of efficiency (merge-sort or quick-sort depending on browser).
-// The split() and join() methods each add one loop, but it they are not nested, so still efficient.
+// The array methods each add one loop, but it they are not nested, so still efficient.
 const sortByStrings = function(unsortedString, keyString) {
   return unsortedString
     .split("")
